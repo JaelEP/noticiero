cd760d91e9ec3daaed8a4d51b2e8af5d784821ed
JaelEP <rismael268@gmail.com>
2025-11-03 17:24:44 -0500

    Update nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -7,5 +7,6 @@ $nacional = [
 "titulo" => "Canciller Hugo de Zela anunció que Betssy Chávez se encuentra asilada en la embajada de México en Lima",
 "autor" => "Jael Estefanero Palacios",
 "resumen" => "El ministro de Relaciones Exteriores indicó que, a raíz de ese hecho, se ha decidido romper relaciones diplomáticas con la nación mexicana.",
+"img" => "https://gestion.pe/resizer/v2/LQYAT3A7URAC5K5WAUHZ5TQOY4.jpg?auth=0630e33155e91b2e084d0ec61d0dfa1f17261edbdb8724a40ab29d4168a3e190&width=1200&height=835&quality=75&smart=true",
 ];
 ?>
