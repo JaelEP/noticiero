6e487cef94872fe899d54db33066f8bc7f8e5f4b
JaelEP <rismael268@gmail.com>
2025-11-03 16:55:44 -0500

    Update portada.php

--- api/secciones/portada.php
+++ api/secciones/portada.php
@@ -6,6 +6,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 $portada = [
 "titulo" => "Portada",
 "autor" => "jperez",
-"resumen" => "Las noticias mas importantes del 2025",
+"resumen" => "Jael Andres Estefanero Palacios",
 ];
 ?>
+
