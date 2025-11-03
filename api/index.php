368c2c00994140bfb7ca2ad8c5b798276d5fa4b6
JaelEP <rismael268@gmail.com>
2025-11-03 17:07:18 -0500

    Update index.php

--- api/index.php
+++ api/index.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" type="image/png" href="img/icono.png">
-
+  <link rel="icon" type="image/png" href="api/img/icono.png">
+    
     <title>DIARIO EL HOCICÓN</title>
 
     <!-- Bootstrap core CSS -->
@@ -579,6 +579,7 @@
     </script>
   </body>
 </html>
+
 
 
 
