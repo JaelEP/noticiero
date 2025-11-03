075a1953f4ec3b7a109346d25b719e082deee5ab
JaelEP <rismael268@gmail.com>
2025-11-03 18:52:59 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -4,8 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "CADE Ejecutivos 2025 confirma la presencia de presidente José Jerí en la sesión inaugural",
+"autor" => "Luis Torres",
+"resumen" => "El foro empresarial contará también con la participación de la titular del Ministerio de Economía y Finanzas (MEF), Denisse Miralles, quien hablará sobre crecimiento económico.",
+"img" => "https://elcomercio.pe/resizer/v2/VIGT4HFORRCM5OSGTBRDOPTNAQ.jpg?auth=9d0600899c4b1045086d905ea14b7b7c1d5e343a618edd883ec2c3f46c062452&width=1200&height=780&quality=75&smart=true",
 ];
 ?>
