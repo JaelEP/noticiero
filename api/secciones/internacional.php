68f5bd4f3ab5c86bcd559dc8115d0c1058c57892
JaelEP <rismael268@gmail.com>
2025-11-03 17:02:09 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "OpenAI continúa su frenesí de compras con multimillonario contrato con Amazon",
+"autor" => "Jael Estefanero Palacios",
+"resumen" => "OpenAI aspira a ser el primer actor del sector en desarrollar un modelo de IA general, o IAG, que iguale el conjunto de las capacidades intelectuales humanas.",
 ];
 ?>
