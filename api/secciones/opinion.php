059d0e460af5a396faaaa8e754b3e07346c20b29
JaelEP <rismael268@gmail.com>
2025-11-03 18:54:22 -0500

    Update opinion.php

--- api/secciones/opinion.php
+++ api/secciones/opinion.php
@@ -4,8 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $opinion = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Los muertos del cambio climático",
+"autor" => "Torres Ramos",
+"resumen" => "La falta de acciones contundentes contra el calentamiento global pone en riesgo la vida de millones de personas",
+"img" => "https://imagenes.elpais.com/resizer/v2/NI5JBMNFDRJGBPU6Z4MBCEXFHE.jpg?auth=c13dd4b032934ae168184ca33a5c0cb97f5e9837bc3562de3ffcf1aa575a9071&width=1000",
 ];
 ?>
