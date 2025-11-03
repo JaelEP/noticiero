834e7ff40d3d4c7b752459199721d51b44d7b7d9
JaelEP <rismael268@gmail.com>
2025-11-03 16:57:40 -0500

    Update nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Canciller Hugo de Zela anunció que Betssy Chávez se encuentra asilada en la embajada de México en Lima",
+"autor" => "Jael Estefanero Palacios",
+"resumen" => "El ministro de Relaciones Exteriores indicó que, a raíz de ese hecho, se ha decidido romper relaciones diplomáticas con la nación mexicana.",
 ];
 ?>
