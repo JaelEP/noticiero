ba1c25ffbdd454e8760502c0e0141ddc9ef3717c
JaelEP <rismael268@gmail.com>
2025-11-03 17:26:44 -0500

    Update index.php

--- api/index.php
+++ api/index.php
@@ -8,7 +8,7 @@
     <meta name="author" content="">
     <link rel="icon" href="https://getbootstrap.com/docs/5.2/assets/img/favicons/android-chrome-192x192.png" type="image/x-icon">
     
-    <title>DIARIO EL HOCICÓN</title>
+    <title>DIARIO 123</title>
 
     <!-- Bootstrap core CSS -->
     <link href="https://getbootstrap.com/docs/4.1/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -584,6 +584,7 @@
     </script>
   </body>
 </html>
+
 
 
 
