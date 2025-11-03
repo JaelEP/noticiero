c7cfe565151fe5da1d236d2552c28fa1e94e0a53
JaelEP <rismael268@gmail.com>
2025-11-03 17:20:52 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -7,5 +7,6 @@ $internacional = [
 "titulo" => "OpenAI continúa su frenesí de compras con multimillonario contrato con Amazon",
 "autor" => "Jael Estefanero Palacios",
 "resumen" => "OpenAI aspira a ser el primer actor del sector en desarrollar un modelo de IA general, o IAG, que iguale el conjunto de las capacidades intelectuales humanas.",
+"img" => "https://gestion.pe/resizer/v2/LQYAT3A7URAC5K5WAUHZ5TQOY4.jpg?auth=0630e33155e91b2e084d0ec61d0dfa1f17261edbdb8724a40ab29d4168a3e190&width=1200&height=835&quality=75&smart=true",
 ];
 ?>
