5f220cec45fca06199ae48085411af6a12b67edf
JaelEP <rismael268@gmail.com>
2025-11-03 19:05:03 -0500

    Update ciencia.php

--- api/secciones/ciencia.php
+++ api/secciones/ciencia.php
@@ -4,8 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $ciencia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Christer Fuglesang: “Podremos llevar personas a Marte en un plazo de 10 a 15 años, si realmente nos esforzamos”",
+"autor" => "Valdivieso Julio",
+"resumen" => "El primer astronauta sueco, participante en dos misiones a la Estación Espacial Internacional, visitó nuestro país para resaltar la importancia de la exploración espacial en el futuro de la humanidad.",
+"img" => "https://elcomercio.pe/resizer/v2/O6FSLTAVPBFNLPLFHPPFJIELGY.jpg?auth=0b9eb1adfc88f5b67055ed15e28dafe041e7783e6efc8dc3b057ac03a1fad79b&width=680&height=680&quality=75&smart=true",
 ];
 ?>
