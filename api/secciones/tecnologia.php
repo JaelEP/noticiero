859f191b40c60523a521610c44a4c814ae67cb33
JaelEP <rismael268@gmail.com>
2025-11-03 19:02:22 -0500

    Update tecnologia.php

--- api/secciones/tecnologia.php
+++ api/secciones/tecnologia.php
@@ -4,8 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $tecnologia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Datos y drones como “extintor digital” de incendios forestales",
+"autor" => "Guerra Luis",
+"resumen" => "La carrera tecnológica para hacer frente a los incendios forestales se traslada a los aviones sin tripulación y a la gestión cada vez más orquestada de los datos.",
+"img" => "https://www.imnovation-hub.com/content/dam/imnovationhub/ciencia-y-tecnologia/tecnologia-incendios-forestales-portada.jpg",
 ];
 ?>
